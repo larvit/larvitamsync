54fdaed8e479e5b70a28b83ebe6237d7d41fbccf
lilleman <lilleman@larvit.se>
2016-12-23 12:09:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build Status](https://travis-ci.org/larvit/larvitamsync.svg?branch=master)](https://travis-ci.org/larvit/larvitamsync) [![Dependencies](https://david-dm.org/larvit/larvitamsync.svg)](https://david-dm.org/larvit/larvitamsync.svg)
+
+# larvitamsync
+
+Sync data between minions
+
+## Usage
---
+++ test/00test.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const	Intercom	= require('larvitamintercom'),
+	lUtils	= require('larvitutils'),
+	async	= require('async'),
+	log	= require('winston'),
+	db	= require('larvitdb'),
+	fs	= require('fs');
+
+
+// Set up winston
+log.remove(log.transports.Console);
+/**/log.add(log.transports.Console, {
+	'level':	'warn',
+	'colorize':	true,
+	'timestamp':	true,
+	'json':	false
+});/**/
+
+before(function(done) {
+	this.timeout(10000);
+	const	tasks	= [];
+
+	let	intercomConfigFile;
+
+	// Run DB Setup
+	tasks.push(function(cb) {
+		let confFile;
+
+		if (process.env.DBCONFFILE === undefined) {
+			confFile = __dirname + '/../config/db_test.json';
+		} else {
+			confFile = process.env.DBCONFFILE;
+		}
+
+		log.verbose('DB config file: "' + confFile + '"');
+
+		// First look for absolute path
+		fs.stat(confFile, function(err) {
+			if (err) {
+
+				// Then look for this string in the config folder
+				confFile = __dirname + '/../config/' + confFile;
+				fs.stat(confFile, function(err) {
+					if (err) throw err;
+					log.verbose('DB config: ' + JSON.stringify(require(confFile)));
+					db.setup(require(confFile), cb);
+				});
+
+				return;
+			}
+
+			log.verbose('DB config: ' + JSON.stringify(require(confFile)));
+			db.setup(require(confFile), cb);
+		});
+	});
+
+	// Check for empty db
+	tasks.push(function(cb) {
+		db.query('SHOW TABLES', function(err, rows) {
+			if (err) throw err;
+
+			if (rows.length) {
+				throw new Error('Database is not empty. To make a test, you must supply an empty database!');
+			}
+
+			cb();
+		});
+	});
+
+	// Setup intercom
+	tasks.push(function(cb) {
+		if (process.env.INTCONFFILE === undefined) {
+			intercomConfigFile = __dirname + '/../config/amqp_test.json';
+		} else {
+			intercomConfigFile = process.env.INTCONFFILE;
+		}
+
+		log.verbose('Intercom config file: "' + intercomConfigFile + '"');
+
+		// First look for absolute path
+		fs.stat(intercomConfigFile, function(err) {
+			if (err) {
+
+				// Then look for this string in the config folder
+				intercomConfigFile = __dirname + '/../config/' + intercomConfigFile;
+				fs.stat(intercomConfigFile, function(err) {
+					if (err) throw err;
+					log.verbose('Intercom config: ' + JSON.stringify(require(intercomConfigFile)));
+					lUtils.instances.intercom = new Intercom(require(intercomConfigFile).default);
+					lUtils.instances.intercom.on('ready', cb);
+				});
+
+				return;
+			}
+
+			log.verbose('Intercom config: ' + JSON.stringify(require(intercomConfigFile)));
+			lUtils.instances.intercom = new Intercom(require(intercomConfigFile).default);
+			lUtils.instances.intercom.on('ready', cb);
+		});
+	});
+
+	async.series(tasks, done);
+});
+
+describe('foo', function() {
+	it('bar', function(done) {
+		setTimeout(function() {
+			done();
+		}, 19);
+	});
+});
+
+after(function(done) {
+	db.removeAllTables(done);
+});
